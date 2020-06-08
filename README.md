26c4e9485f5f2a0a2faeed7e2048ec513b51c269
icyboy <xupeng.js@gmail.com>
2020-06-08 16:33:41 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# go mod
+cd ~/work/github/kubernetes/staging/src/k8s.io
+ls | xargs -n 1 -I{} bash -c "curl -s https://proxy.golang.org/k8s.io/{}/@v/kubernetes-1.16.10.info | jq -r '\"k8s.io/{} => k8s.io/{} \(.Version)\"'"
+
 # sample-scheduler-framework
 
 This repo is a sample for Kubernetes scheduler framework.
--- pkg/plugins/sample-plugin.go
+++ pkg/plugins/sample-plugin.go
@@ -2,49 +2,73 @@ package plugins
 
 import (
 	"fmt"
+
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/klog"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
 )
 
-// 插件名称
+// Name 插件名称
 const Name = "sample-plugin"
 
+//Args ...
 type Args struct {
 	FavoriteColor  string `json:"favorite_color,omitempty"`
 	FavoriteNumber int    `json:"favorite_number,omitempty"`
 	ThanksTo       string `json:"thanks_to,omitempty"`
 }
 
+//Sample ...
 type Sample struct {
 	args   *Args
 	handle framework.FrameworkHandle
 }
 
+//Name ...
 func (s *Sample) Name() string {
 	return Name
 }
 
-func (s *Sample) PreFilter(pc *framework.PluginContext, pod *v1.Pod) *framework.Status {
+//PreFilter ...
+func (s *Sample) PreFilter(pc *framework.CycleState, pod *v1.Pod) *framework.Status {
 	klog.V(3).Infof("prefilter pod: %v", pod.Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
-func (s *Sample) Filter(pc *framework.PluginContext, pod *v1.Pod, nodeName string) *framework.Status {
+//Filter ...
+func (s *Sample) Filter(pc *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
 	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName)
 	return framework.NewStatus(framework.Success, "")
 }
 
-func (s *Sample) PreBind(pc *framework.PluginContext, pod *v1.Pod, nodeName string) *framework.Status {
-	if nodeInfo, ok := s.handle.NodeInfoSnapshot().NodeInfoMap[nodeName]; !ok {
-		return framework.NewStatus(framework.Error, fmt.Sprintf("prebind get node info error: %+v", nodeName))
-	} else {
-		klog.V(3).Infof("prebind node info: %+v", nodeInfo.Node())
-		return framework.NewStatus(framework.Success, "")
-	}
+//Score ...
+func (s *Sample) Score(pc *framework.CycleState, pod *v1.Pod, nodeName string) (int, *framework.Status) {
+	klog.V(3).Infof("Score pod: %v, node: %v", pod.Name, nodeName)
+	score := rand.Intn(100)
+	return score, framework.NewStatus(framework.Success, "")
 }
 
+//NormalizeScore ...
+func (s *Sample) NormalizeScore(pc *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+	klog.V(3).Infof("NormalizeScore pod: %v, score: %s", pod.Name, scores)
+	return framework.NewStatus(framework.Success, "")
+}
+
+//PreBind ...
+func (s *Sample) PreBind(pc *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
+	nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
+	if err != nil {
+		return framework.NewStatus(framework.Error, fmt.Sprintf("prebind get node info error: %+v", nodeName))
+	}
+
+	klog.V(3).Infof("prebind node info: %+v", nodeInfo.Node())
+	return framework.NewStatus(framework.Success, "")
+
+}
+
+//New ...
 //type PluginFactory = func(configuration *runtime.Unknown, f FrameworkHandle) (Plugin, error)
 func New(configuration *runtime.Unknown, f framework.FrameworkHandle) (framework.Plugin, error) {
 	args := &Args{}
@@ -53,7 +77,7 @@ func New(configuration *runtime.Unknown, f framework.FrameworkHandle) (framework
 	}
 	klog.V(3).Infof("get plugin config args: %+v", args)
 	return &Sample{
-		args: args,
+		args:   args,
 		handle: f,
 	}, nil
 }
